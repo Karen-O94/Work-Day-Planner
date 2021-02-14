63fb6159e3a49124fa8c68b78bf98b0665a26d79
Karen Opoku <karenyaa@live.co.uk>
2021-02-14 18:13:49 +0000

    fixed issue with formatRows function

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
                 
                  // adds colour red if timeblock matches current time
                 if (targetRow === currentHour) { 
-                    $(this).addClass("present").removeClass("past future");
+                    $(this).addClass("present").removeClass("past future";
                 }  // adds colour grey if timeblock occured already (past event)
                 else if (targetRow < currentHour) {
                     $(this).addClass("past").removeClass("present future");
